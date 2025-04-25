e9a3c92dbafdfc0083d8592364dbd0371caf29ad
Jaimz <jaimz@gmail.com>
2025-04-26 00:07:50 +0100

    deploy2

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+// const API_URL = "http://localhost:5000";
+const API_URL = https://task-gpt-be.onrender.com
 
 export const generateTasks = async (prompt) => {
   try {
